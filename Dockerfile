a784b234ecdf3175568d4fea64cf3ee9bce54a2e
Marcus Bergo <eu@mbergo.dev>
2023-01-22 19:28:52 -0300

    Creating bot for discord

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.9
+
+# create and set working directory
+RUN mkdir -p /app
+WORKDIR /app
+
+# copy requirements file
+COPY requirements.txt /app
+
+# install dependencies
+RUN pip install --upgrade pip
+RUN pip install -r requirements.txt
+
+# copy the rest of the files
+COPY . /app
+
+# run the bot
+CMD ["python3", "bot.py"]
+
---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import openai
+
+# Get the Discord bot token and GPT-2 API key from the openai_secret_manager
+discord_token = "<YOUR_DISCORD_TOKEN>"
+bot = commands.Bot(command_prefix='#', description="GPT-2 Discord Bot", intents=discord.Intents.all())
+
+@bot.command()
+async def talk(ctx, *, message):
+    # Use the GPT-2 API to generate a response
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"{message}\n",
+        max_tokens=1024,
+        n = 1,
+        stop=None,
+        temperature=0.5,
+        api_key = "<YOUR_API_KEY>"
+    )
+
+    # Send the response to the Discord channel
+    await ctx.send(response.choices[0].text)
+
+bot.run(discord_token)
