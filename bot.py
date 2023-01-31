4d0d12951f185a95965b205b498598bdc2c812f5
Marcus Bergo <eu@mbergo.dev>
2023-01-31 01:57:38 -0300

    Refactor whatsapp method and add create group and job title

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ async def predy_whatsapp(ctx, to, message):
         "Content-Type": "application/json"
     }
     # Replace YOUR_WHATSAPP_NUMBER with your WhatsApp Business number
-    from_number = "YOUR_WHATSAPP_NUMBER"
+    from_number = "+5521982083167"
     data = {
         "from": from_number,
         "to": to,
@@ -65,5 +65,40 @@ async def predy_whatsapp(ctx, to, message):
     else:
         await ctx.send(f"Erro ao enviar mensagem para {to}: {response.text}")
 
+# Criar canais
+@bot.command()
+async def predy_criar_grupo(ctx, team_name, *participants):
+    guild = ctx.guild
+    existing_role = discord.utils.get(guild.roles, name=team_name)
+    if not existing_role:
+        new_role = await guild.create_role(name=team_name)
+        await new_role.edit(mentionable=True)
+    else:
+        new_role = existing_role
+    participants = [p.strip() for p in participants]
+    member_objects = [discord.utils.get(guild.members, name=p) for p in participants]
+    member_objects = [m for m in member_objects if m is not None]
+    if not member_objects:
+        await ctx.send(f"Nenhum participante foi encontrado.")
+    else:
+        for member in member_objects:
+            await member.add_roles(new_role)
+        await ctx.send(f"Grupo {team_name} criado com os seguintes participantes: {', '.join([m.name for m in member_objects])}.")
 
-bot.run(discord_token)
+# Adicionar cargo
+@bot.command()
+async def predy_adicionar_cargo(ctx, member: discord.Member, job_title: str):
+    guild = ctx.guild
+    job_title = job_title.capitalize()
+    
+    # Check if the job title role exists, and create it if it doesn't
+    role = discord.utils.get(guild.roles, name=job_title)
+    if role is None:
+        role = await guild.create_role(name=job_title)
+    
+    # Add the role to the member
+    await member.add_roles(role)
+    
+    await ctx.send(f"{member.mention} was assigned the role {role.name}.")
+
+@bot.run(discord_token)
